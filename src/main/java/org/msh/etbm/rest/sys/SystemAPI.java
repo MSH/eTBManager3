7b20ef97b6b789b8514a7cb14f5eb7be1b5f4092
Ricardo Memoria <rmemoria@gmail.com>
2015-08-16 12:29:02 -0300

    * Service - Renaming classes

--- src/main/java/org/msh/etbm/rest/sys/SystemAPI.java
+++ src/main/java/org/msh/etbm/rest/sys/SystemAPI.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/sys")
-public class SystemStatusAPI {
+public class SystemAPI {
 
     @Autowired
     SystemService systemService;
