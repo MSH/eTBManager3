08445866ddf33221be6e5827807e9c8af5879142
Ricardo Memoria <rmemoria@gmail.com>
2015-08-22 00:42:53 -0300

    * Rest API - api/init/workspace - Register workspace (incomplete);

---
+++ src/main/java/org/msh/etbm/rest/init/InitWorkspaceForm.java
@@ -0,0 +1,44 @@
+package org.msh.etbm.rest.init;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by rmemoria on 22/8/15.
+ */
+public class InitWorkspaceForm {
+
+    /**
+     * Workspace name
+     */
+    @NotNull
+    private String wsName;
+
+    /**
+     * Administrator password
+     */
+    @NotNull
+    private String adminPassword;
+
+    /**
+     * Administrator e-mail address
+     */
+    @NotNull
+    private String adminEmail;
+
+
+    public String getWsName() {
+        return wsName;
+    }
+
+    public void setWsName(String wsName) {
+        this.wsName = wsName;
+    }
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+}
---
+++ src/main/java/org/msh/etbm/rest/init/InitWorkspaceRest.java
@@ -0,0 +1,21 @@
+package org.msh.etbm.rest.init;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * Created by rmemoria on 22/8/15.
+ */
+@RestController
+@RequestMapping("/api/init")
+public class InitWorkspaceRest {
+
+    @RequestMapping(value = "/workspace", method = RequestMethod.POST)
+    public boolean registerWorkspace(@Valid @RequestBody InitWorkspaceForm frm) {
+        return false;
+    }
+}
