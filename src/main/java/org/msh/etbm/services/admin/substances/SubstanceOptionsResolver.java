c68c06e8e657addea5c9b6c16abb45cc79368b61
Ricardo Memoria <rmemoria@gmail.com>
2016-02-02 22:31:36 -0200

    * Substance options - Including short name in the list;

--- src/main/java/org/msh/etbm/services/admin/substances/SubstanceOptionsResolver.java
+++ src/main/java/org/msh/etbm/services/admin/substances/SubstanceOptionsResolver.java
@@ -1,6 +1,7 @@
 package org.msh.etbm.services.admin.substances;
 
 import org.msh.etbm.commons.Item;
+import org.msh.etbm.commons.SynchronizableItem;
 import org.msh.etbm.commons.entities.query.QueryResult;
 import org.msh.etbm.commons.forms.options.OptionsManagerService;
 import org.msh.etbm.commons.forms.options.OptionsResolver;
@@ -8,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * Provide a list of options for substances
  * Created by rmemoria on 1/2/16.
  */
 @Component
@@ -38,10 +41,16 @@ public class SubstanceOptionsResolver implements OptionsResolver {
     @Override
     public List<Item> getOptions(Map<String, Object> params) {
         SubstanceQueryParams qry = new SubstanceQueryParams();
-        qry.setProfile(SubstanceQueryParams.PROFILE_ITEM);
+        qry.setProfile(SubstanceQueryParams.PROFILE_DEFAULT);
         qry.setOrderBy(SubstanceQueryParams.ORDERBY_NAME);
-        QueryResult res = substanceService.findMany(qry);
+        QueryResult<SubstanceData> res = substanceService.findMany(qry);
 
-        return res.getList();
+        // mount list to include the short name in the label
+        List<Item> lst = new ArrayList<>();
+        for (SubstanceData sub: res.getList()) {
+            lst.add(new SynchronizableItem(sub.getId(), "(" + sub.getShortName() + ") " + sub.getName()));
+        }
+
+        return lst;
     }
 }
