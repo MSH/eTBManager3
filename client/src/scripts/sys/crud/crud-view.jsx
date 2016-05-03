773899e2f7712a3017eb2c95a311039ba31e577b
Ricardo Memoria <rmemoria@gmail.com>
2016-05-03 07:47:19 -0300

    UI - Case issues (mock data)

--- client/src/scripts/sys/crud/crud-view.jsx
+++ client/src/scripts/sys/crud/crud-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Badge } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { Card } from '../../components';
 import { hasPerm } from '../session';
 import CrudMessage from './crud-message';
