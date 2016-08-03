ec1094e5f5d41c945f8929f1dd091ab5e723c012
Mauricio <mauriciojdsantos@gmail.com>
2016-08-03 18:11:12 -0300

    Case Issues UI improvements

--- client/src/scripts/sys/home/cases/case-issues.jsx
+++ client/src/scripts/sys/home/cases/case-issues.jsx
@@ -26,6 +26,84 @@ const editorDef = {
 	title: __('cases.issues.new')
 };
 
+const mockIssues = [
+	{
+		id: '12345-2',
+		title: 'Thia case is no TB',
+		description: 'patient didn\'t receive any attention from the primary health care service and now he is complaining about the treatment.\n' +
+		'Please ask recommendation about what to do.\n\nRegards\nTreatment health care',
+		user: {
+			id: '12345-22',
+			name: 'Nicko McBrain'
+		},
+		unit: {
+			id: '12345-22',
+			name: 'Treatment health care'
+		},
+		creationDate: new Date(2015, 2, 3, 12, 35, 55),
+		lastAnswerDate: new Date(2015, 2, 3, 12, 35, 55),
+		closed: false
+	},
+	{
+		id: '12345-1',
+		title: 'Missing parent name',
+		description: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+		user: {
+			id: '12345-22',
+			name: 'Dave Murray'
+		},
+		unit: {
+			id: '12345-22',
+			name: 'Hospital do pulmão'
+		},
+		closed: true,
+		creationDate: new Date(2015, 1, 1, 5, 20, 5),
+		lastAnswerDate: new Date(2015, 2, 3, 12, 35, 55),
+		followups: [
+			{
+				id: '54333-1',
+				text: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+				user: {
+					id: '12345-22',
+					name: 'Dave Murray'
+				},
+				unit: {
+					id: '12345-22',
+					name: 'UNIT2asdasd'
+				},
+				followupDate: new Date(2015, 1, 1, 5, 20, 5)
+			},
+			{
+				id: '54333-2',
+				text: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+				user: {
+					id: '12345-22',
+					name: 'Dave Murray'
+				},
+				unit: {
+					id: '12345-22',
+					name: 'UNIT2asdasd'
+				},
+				followupDate: new Date(2015, 1, 1, 5, 20, 5)
+			},
+			{
+				id: '54333-3',
+				text: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into',
+				user: {
+					id: '12345-22',
+					name: 'Dave Murray'
+				},
+				unit: {
+					id: '12345-22',
+					name: 'UNIT2asdasd'
+				},
+				followupDate: new Date(2015, 1, 1, 5, 20, 5)
+			}
+		]
+	}
+];
+
+
 export default class CaseIssues extends React.Component {
 
 	constructor(props) {
@@ -116,7 +194,7 @@ export default class CaseIssues extends React.Component {
 	}
 
 	render() {
-		const issues = this.props.tbcase.issues;
+		const issues = mockIssues;
 
 		// choose a message to dsplay at the top
 		const openIssues = issues ? issues.find(issue => !issue.closed) : null;
