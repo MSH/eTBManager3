9bb995592c2c963f389a52a0eb13b1af30ca1a63
Ricardo Memoria <rmemoria@gmail.com>
2016-08-11 21:18:11 -0300

    UI - Treatment followup - 1st version

---
+++ client/src/scripts/sys/home/cases/treat/calend-editor.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
+import { Popup, Fa } from '../../../../components';
+import FollowupCalendar from './followup-calendar';
+
+
+/**
+ * Display the treatment followup card
+ */
+export default class CalendEditor extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this._handleDayClick = this._handleDayClick.bind(this);
+		this._buttonClick = this._buttonClick.bind(this);
+		this.cancelClick = this.cancelClick.bind(this);
+		this.saveClick = this.saveClick.bind(this);
+		this.selectAll = this.selectAll.bind(this);
+
+		this.state = { selBtn: 'DOTS' };
+	}
+
+	componentWillMount() {
+		const days = this.props.data.days ? this.props.data.days.slice(0) : [];
+		const data = Object.assign({}, this.props.data, { days: days });
+		this.setState({ data: data });
+	}
+
+
+	_handleDayClick(e, dt) {
+		const days = this.state.data.days;
+		const day = dt.getDate();
+		let item = days.find(it => it.day === day);
+		const status = this.state.selBtn;
+
+		if (item) {
+			if (item.status === status) {
+				const index = days.indexOf(item);
+				days.splice(index, 1);
+			} else {
+				item.status = status;
+			}
+		} else {
+			item = { day: day, status: status };
+			days.push(item);
+		}
+
+		this.setState({ data: Object.assign({}, this.state.data) });
+	}
+
+	cancelClick() {
+		this._close();
+	}
+
+	saveClick() {
+		this._close(this.state.data);
+	}
+
+	_close(data) {
+		if (this.props.onClose) {
+			this.props.onClose(data);
+		}
+	}
+
+	/**
+	 * Select all days with the same state
+	 * @return {[type]} [description]
+	 */
+	selectAll() {
+		const data = this.state.data;
+
+		// get number of days in a month
+		const dt = new Date(data.year, data.month + 1, 0);
+		const num = dt.getDate();
+
+		const days = [];
+		const status = this.state.selBtn;
+
+		for (var i = 1; i <= num; i++) {
+			days.push({ day: i, status: status });
+		}
+
+		this.setState({ data: Object.assign({}, data, { days: days }) });
+	}
+
+	/**
+	 * Called when user clicks on the selection toolbar button
+	 * @param  {[type]} key [description]
+	 * @return {[type]}     [description]
+	 */
+	_buttonClick(key) {
+		return () => {
+			this.setState({ selBtn: key });
+		};
+	}
+
+
+	_renderToolbar() {
+		const active = this.state.selBtn;
+
+		return (
+			<div style={{ padding: '4px' }}>
+				<ButtonGroup bsSize="small" vertical block>
+					<Button active={active === 'DOTS'} onClick={this._buttonClick('DOTS')}>
+						<Fa icon="circle" className="text-success"/>{__('TreatmentDayOption.DOTS')}
+					</Button>
+					<Button active={active === 'SELF_ADMIN'} onClick={this._buttonClick('SELF_ADMIN')}>
+						<Fa icon="circle" className="text-primary"/>{__('TreatmentDayOption.SELF_ADMIN')}
+					</Button>
+					<Button active={active === 'NOT_TAKEN'} onClick={this._buttonClick('NOT_TAKEN')}>
+						<Fa icon="circle" className="text-muted"/>{__('TreatmentDayOption.NOT_TAKEN')}
+					</Button>
+				</ButtonGroup>
+				<a className="text-small" onClick={this.selectAll}>{__('action.selectall')}</a>
+			</div>
+			);
+	}
+
+
+	render() {
+		const data = this.state.data;
+
+		return (
+			<div onClick={this._calendarClick}>
+				<Popup show>
+					<div className="editor">
+						{this._renderToolbar()}
+						<FollowupCalendar
+							data={data}
+							onDayClick={this._handleDayClick}
+							/>
+						<ButtonToolbar style={{ margin: '4px' }}>
+							<Button bsStyle="success"
+								bsSize="small"
+								onClick={this.saveClick}>
+								{__('action.save')}
+							</Button>
+							<Button bsStyle="link"
+								bsSize="small"
+								onClick={this.cancelClick}>
+								{__('action.cancel')}
+							</Button>
+						</ButtonToolbar>
+					</div>
+				</Popup>
+			</div>
+			);
+	}
+}
+
+CalendEditor.propTypes = {
+	data: React.PropTypes.object.isRequired,
+	onClose: React.PropTypes.func
+};
--- client/src/scripts/sys/home/cases/treat/followup-calendar.jsx
+++ client/src/scripts/sys/home/cases/treat/followup-calendar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DayPicker, { DateUtils } from 'react-day-picker';
+import DayPicker from 'react-day-picker';
 
 
 /**
@@ -9,17 +9,27 @@ export default class FollowupCalendar extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this._handleDayClick = this._handleDayClick.bind(this);
+		this._calendarClick = this._calendarClick.bind(this);
+
+		this.state = { selBtn: 'DOTS' };
 	}
 
 	shouldComponentUpdate(nextProps) {
 		return this.props.data !== nextProps.data;
 	}
 
-	_handleDayClick(e, day) {
-		this.props.onClick(day, this.props.data);
+	_calendarClick() {
+		if (this.props.onClick) {
+			this.props.onClick(this.props.data);
+		}
 	}
 
+	/**
+	 * Return the status of the given by according to the data being edited
+	 * @param  {[type]} data [description]
+	 * @param  {[type]} dt   [description]
+	 * @return {[type]}      [description]
+	 */
 	_dayStatus(dt) {
 		const days = this.props.data.days;
 		if (!days) {
@@ -29,35 +39,44 @@ export default class FollowupCalendar extends React.Component {
 		const day = dt.getDate();
 
 		const item = days.find(it => it.day === day);
+
 		return item ? item.status : null;
 	}
 
+	/**
+	 * Render the day in the calendar to easy styling
+	 * @param  {[type]} dt [description]
+	 * @return {[type]}    [description]
+	 */
 	_renderDay(dt) {
 		const day = dt.getDate();
 
 		return <div className="day">{day}</div>;
 	}
 
+
 	render() {
 		const data = this.props.data;
 		const year = data.year;
 		const month = data.month;
 		const day = data.iniDay ? data.iniDay : 1;
-		const dt = new Date(year, month, day, 0, 0);
+		const inidt = new Date(year, month, day, 0, 0);
 
 		const modifiers = {
-			dots: date => this._dayStatus(date) === 'DOTS',
-			selfadmin: date => this._dayStatus(date) === 'SELF_ADMIN',
-			nottaken: date => this._dayStatus(date) === 'NOT_TAKEN'
+				dots: dt => this._dayStatus(dt) === 'DOTS',
+				selfadmin: dt => this._dayStatus(dt) === 'SELF_ADMIN',
+				nottaken: dt => this._dayStatus(dt) === 'NOT_TAKEN'
 		};
 
 		return (
-			<div className="treat-cal">
+			<div className={this.props.className} onClick={this._calendarClick}>
 				<DayPicker canChangeMonth={false}
-					initialMonth={dt}
+					initialMonth={inidt}
 					modifiers={modifiers}
 					renderDay={this._renderDay}
-					onDayClick={this._handleDayClick} />
+					onDayClick={this.props.onDayClick}
+				/>
+				{this.props.children}
 			</div>
 			);
 	}
@@ -65,5 +84,8 @@ export default class FollowupCalendar extends React.Component {
 
 FollowupCalendar.propTypes = {
 	data: React.PropTypes.object.isRequired,
-	onClick: React.PropTypes.func.isRequired
+	onClick: React.PropTypes.func,
+	onDayClick: React.PropTypes.func,
+	className: React.PropTypes.string,
+	children: React.PropTypes.any
 };
