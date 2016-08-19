c5d60e4666135652a0118d716db18a8818f5a6d2
Mauricio <mauriciojdsantos@gmail.com>
2016-08-18 21:21:10 -0300

    final implementation case comorbidities UI

--- client/src/scripts/sys/home/cases/case-prev-tb-treats.jsx
+++ client/src/scripts/sys/home/cases/case-prev-tb-treats.jsx
@@ -9,65 +9,79 @@ import { Profile } from '../../../components';
 
 const crud = new CRUD('prevtreat');
 
-const displaySchema1 = {
+const readOnlySchema = {
 			controls: [
 				{
-					type: 'string',
-					label: __('cases.prevtreat.iniyear'),
-					property: 'year',
-					size: { sm: 2 }
+					property: 'am',
+					type: 'yesNo',
+					label: __('cases.prevtreat.am'),
+					size: { sm: 1 }
 				},
 				{
-					type: 'select',
-					property: 'month',
-					label: __('cases.prevtreat.inimonth'),
-					options: [
-						{ id: 0, name: 'Janeiro' },
-						{ id: 1, name: 'Fevereiro' },
-						{ id: 2, name: 'Março' },
-						{ id: 3, name: 'Abril' },
-						{ id: 4, name: 'Maio' },
-						{ id: 5, name: 'Junho' },
-						{ id: 6, name: 'Julho' },
-						{ id: 7, name: 'Agosto' },
-						{ id: 8, name: 'Setembro' },
-						{ id: 9, name: 'Outubro' },
-						{ id: 10, name: 'Novembro' },
-						{ id: 11, name: 'Dezembro' }
-					],
-					size: { sm: 2 }
+					property: 'cfz',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cfz'),
+					size: { sm: 1 }
 				},
 				{
-					type: 'string',
-					label: __('cases.prevtreat.endyear'),
-					property: 'outcomeYear',
-					size: { sm: 2 }
+					property: 'cm',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cm'),
+					size: { sm: 1 }
 				},
 				{
-					type: 'select',
-					property: 'outcomeMonth',
-					label: __('cases.prevtreat.endmonth'),
-					options: [
-						{ id: 0, name: 'Janeiro' },
-						{ id: 1, name: 'Fevereiro' },
-						{ id: 2, name: 'Março' },
-						{ id: 3, name: 'Abril' },
-						{ id: 4, name: 'Maio' },
-						{ id: 5, name: 'Junho' },
-						{ id: 6, name: 'Julho' },
-						{ id: 7, name: 'Agosto' },
-						{ id: 8, name: 'Setembro' },
-						{ id: 9, name: 'Outubro' },
-						{ id: 10, name: 'Novembro' },
-						{ id: 11, name: 'Dezembro' }
-					],
-					size: { sm: 2 }
+					property: 'cs',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cs'),
+					size: { sm: 1 }
 				},
 				{
-					type: 'string',
-					label: __('cases.prevtreat.outcome'),
-					property: 'outcome.name',
-					size: { sm: 4 }
+					property: 'e',
+					type: 'yesNo',
+					label: __('cases.prevtreat.e'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'eto',
+					type: 'yesNo',
+					label: __('cases.prevtreat.eto'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'h',
+					type: 'yesNo',
+					label: __('cases.prevtreat.h'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'lfx',
+					type: 'yesNo',
+					label: __('cases.prevtreat.lfx'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'ofx',
+					type: 'yesNo',
+					label: __('cases.prevtreat.ofx'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'r',
+					type: 'yesNo',
+					label: __('cases.prevtreat.r'),
+					size: { sm: 1 }
+				},
+				{
+					property: 's',
+					type: 'yesNo',
+					label: __('cases.prevtreat.s'),
+					size: { sm: 1 }
+				},
+				{
+					property: 'z',
+					type: 'yesNo',
+					label: __('cases.prevtreat.z'),
+					size: { sm: 1 }
 				}
 			]
 		};
@@ -77,7 +91,6 @@ export default class CasePrevTbTreats extends React.Component {
 	constructor(props) {
 		super(props);
 		this.cellRender = this.cellRender.bind(this);
-		this.collapseCellRender = this.collapseCellRender.bind(this);
 
 		const editorSchema = {
 			defaultProperties: {
@@ -86,138 +99,144 @@ export default class CasePrevTbTreats extends React.Component {
 			controls: [
 				{
 					type: 'select',
-					label: __('cases.prevtreat.iniyear'),
-					property: 'year',
-					options: { from: 1990, to: 2016 },
-					size: { sm: 6 }
-				},
-				{
-					type: 'select',
 					property: 'month',
 					label: __('cases.prevtreat.inimonth'),
 					options: [
-						{ id: 0, name: 'Janeiro' },
-						{ id: 1, name: 'Fevereiro' },
-						{ id: 2, name: 'Março' },
-						{ id: 3, name: 'Abril' },
-						{ id: 4, name: 'Maio' },
-						{ id: 5, name: 'Junho' },
-						{ id: 6, name: 'Julho' },
-						{ id: 7, name: 'Agosto' },
-						{ id: 8, name: 'Setembro' },
-						{ id: 9, name: 'Outubro' },
-						{ id: 10, name: 'Novembro' },
-						{ id: 11, name: 'Dezembro' }
+						{ id: 1, name: 'Janeiro' },
+						{ id: 2, name: 'Fevereiro' },
+						{ id: 3, name: 'Março' },
+						{ id: 4, name: 'Abril' },
+						{ id: 5, name: 'Maio' },
+						{ id: 6, name: 'Junho' },
+						{ id: 7, name: 'Julho' },
+						{ id: 8, name: 'Agosto' },
+						{ id: 9, name: 'Setembro' },
+						{ id: 10, name: 'Outubro' },
+						{ id: 11, name: 'Novembro' },
+						{ id: 12, name: 'Dezembro' }
 					],
-					size: { sm: 6 }
+					size: { sm: 6 },
+					required: true
 				},
-
-				{
-					property: 'am',
-					type: 'yesNo',
-					label: __('cases.prevtreat.am'),
-					size: { md: 3 }
-				},
-				{
-					property: 'cfz',
-					type: 'yesNo',
-					label: __('cases.prevtreat.cfz'),
-					size: { md: 3 }
-				},
-				{
-					property: 'cm',
-					type: 'yesNo',
-					label: __('cases.prevtreat.cm'),
-					size: { md: 3 }
-				},
-				{
-					property: 'cs',
-					type: 'yesNo',
-					label: __('cases.prevtreat.cs'),
-					size: { md: 3 }
-				},
-				{
-					property: 'e',
-					type: 'yesNo',
-					label: __('cases.prevtreat.e'),
-					size: { md: 3 }
-				},
-				{
-					property: 'eto',
-					type: 'yesNo',
-					label: __('cases.prevtreat.eto'),
-					size: { md: 3 }
-				},
-				{
-					property: 'h',
-					type: 'yesNo',
-					label: __('cases.prevtreat.h'),
-					size: { md: 3 }
-				},
-				{
-					property: 'lfx',
-					type: 'yesNo',
-					label: __('cases.prevtreat.lfx'),
-					size: { md: 3 }
-				},
-				{
-					property: 'ofx',
-					type: 'yesNo',
-					label: __('cases.prevtreat.ofx'),
-					size: { md: 3 }
-				},
-				{
-					property: 'r',
-					type: 'yesNo',
-					label: __('cases.prevtreat.r'),
-					size: { md: 3 }
-				},
-				{
-					property: 's',
-					type: 'yesNo',
-					label: __('cases.prevtreat.s'),
-					size: { md: 3 }
-				},
-				{
-					property: 'z',
-					type: 'yesNo',
-					label: __('cases.prevtreat.z'),
-					size: { md: 3 }
-				},
-
 				{
 					type: 'select',
-					label: __('cases.prevtreat.endyear'),
-					property: 'outcomeYear',
+					label: __('cases.prevtreat.iniyear'),
+					property: 'year',
 					options: { from: 1990, to: 2016 },
-					size: { sm: 4 }
+					size: { sm: 6 },
+					required: true
 				},
 				{
 					type: 'select',
 					property: 'outcomeMonth',
 					label: __('cases.prevtreat.endmonth'),
 					options: [
-						{ id: 0, name: 'Janeiro' },
-						{ id: 1, name: 'Fevereiro' },
-						{ id: 2, name: 'Março' },
-						{ id: 3, name: 'Abril' },
-						{ id: 4, name: 'Maio' },
-						{ id: 5, name: 'Junho' },
-						{ id: 6, name: 'Julho' },
-						{ id: 7, name: 'Agosto' },
-						{ id: 8, name: 'Setembro' },
-						{ id: 9, name: 'Outubro' },
-						{ id: 10, name: 'Novembro' },
-						{ id: 11, name: 'Dezembro' }
+						{ id: 1, name: 'Janeiro' },
+						{ id: 2, name: 'Fevereiro' },
+						{ id: 3, name: 'Março' },
+						{ id: 4, name: 'Abril' },
+						{ id: 5, name: 'Maio' },
+						{ id: 6, name: 'Junho' },
+						{ id: 7, name: 'Julho' },
+						{ id: 8, name: 'Agosto' },
+						{ id: 9, name: 'Setembro' },
+						{ id: 10, name: 'Outubro' },
+						{ id: 11, name: 'Novembro' },
+						{ id: 12, name: 'Dezembro' }
 					],
-					size: { sm: 4 }
+					size: { sm: 6 }
+				},
+				{
+					type: 'select',
+					label: __('cases.prevtreat.endyear'),
+					property: 'outcomeYear',
+					options: { from: 1990, to: 2016 },
+					size: { sm: 6 }
 				},
 				{
 					type: 'select',
 					label: __('cases.prevtreat.outcome'),
 					property: 'outcome',
 					options: app.getState().app.lists.PrevTBTreatmentOutcome,
-					size: { sm: 4 }
+					size: { sm: 12 },
+					required: true
+				},
+				{
+					type: 'subtitle',
+					label: __('cases.prevtreat.substances'),
+					size: { sm: 12 }
+				},
+				{
+					property: 'am',
+					type: 'yesNo',
+					label: __('cases.prevtreat.am'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'cfz',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cfz'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'cm',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cm'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'cs',
+					type: 'yesNo',
+					label: __('cases.prevtreat.cs'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'e',
+					type: 'yesNo',
+					label: __('cases.prevtreat.e'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'eto',
+					type: 'yesNo',
+					label: __('cases.prevtreat.eto'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'h',
+					type: 'yesNo',
+					label: __('cases.prevtreat.h'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'lfx',
+					type: 'yesNo',
+					label: __('cases.prevtreat.lfx'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'ofx',
+					type: 'yesNo',
+					label: __('cases.prevtreat.ofx'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'r',
+					type: 'yesNo',
+					label: __('cases.prevtreat.r'),
+					size: { sm: 3 }
+				},
+				{
+					property: 's',
+					type: 'yesNo',
+					label: __('cases.prevtreat.s'),
+					size: { sm: 3 }
+				},
+				{
+					property: 'z',
+					type: 'yesNo',
+					label: __('cases.prevtreat.z'),
+					size: { sm: 3 }
 				}
 			],
 			title: doc => doc && doc.id ? __('cases.prevtreat.edt') : __('cases.prevtreat.new')
@@ -227,14 +246,23 @@ export default class CasePrevTbTreats extends React.Component {
 	}
 
 	cellRender(item) {
-		console.log(item);
+		let title = moment([2000, item.month - 1, 1]).format('MMMM') + '/' + item.year;
+		const outcomeMonth = item.outcomeMonth ? moment([2000, item.outcomeMonth - 1, 1]).format('MMMM') + '/' : null;
+		title = title + (item.outcomeYear ? ' ' + __('global.until') + ' ' + outcomeMonth + item.outcomeYear : null);
+
 		return (
-			<Form readOnly schema={displaySchema1} doc={item} />
+			<span>
+				<div>
+					<b>{title + ' - ' + item.outcome.name}</b>
+				</div>
+				<hr/>
+				<Form readOnly schema={readOnlySchema} doc={item} />
+			</span>
 		);
 	}
 
-	collapseCellRender(item) {
-		return (null);
+	collapseCellRender() {
+		return (<hr/>);
 	}
 
 	render() {
@@ -250,7 +278,7 @@ export default class CasePrevTbTreats extends React.Component {
 					onCellRender={this.cellRender}
 					onDetailRender={this.collapseCellRender}
 					queryFilters={{ tbcaseId: tbcase.id }}
-					cellSize={{ md: 12 }}
+					cellSize={{ sm: 12 }}
 					/>
 			</CaseComments>
 			);
