10c8a3d2c231d1e465d9dc67267e7bb131935e59
Ricardo Memoria <rmemoria@gmail.com>
2016-10-11 10:14:48 -0300

    Server - Fixing Checkstyle code analysis

--- src/main/java/org/msh/etbm/services/cases/filters/CaseFilters.java
+++ src/main/java/org/msh/etbm/services/cases/filters/CaseFilters.java
@@ -56,7 +56,6 @@ public class CaseFilters {
 
         grp.add(new EnumFilter(CASE_CLASSIFICATION,
                 CaseClassification.class, "${CaseClassification}", "tbcase.classification"));
-//        grp.add(new GenderFilter());
         grp.add(new ModelFieldOptionsFilter(GENDER, "${Gender}", "patient", "gender"));
         grp.add(new EnumFilter(CASE_STATE, CaseState.class, "${CaseState}", "tbcase.state"));
         grp.add(new EnumFilter(INFECTION_SITE, InfectionSite.class, "${InfectionSite}", "tbcase.infectionSite"));
